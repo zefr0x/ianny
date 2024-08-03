1ac72f101f74778621689dbbdcb43f58735ec68c
zefr0x <zer0-x.7ty50@aleeas.com>
2024-08-03 17:47:06 +0300

    wayland: handle `GlobalRemove` wl_registry event

--- src/wayland.rs
+++ src/wayland.rs
@@ -13,8 +13,11 @@ pub enum Signal {
     Resumed,
 }
 
+type GlobalName = u32;
+
 pub struct State {
-    idle_notifier: Option<ext_idle_notifier_v1::ExtIdleNotifierV1>,
+    idle_notifier: Option<(GlobalName, ext_idle_notifier_v1::ExtIdleNotifierV1)>,
+    idle_notification: Option<ext_idle_notification_v1::ExtIdleNotificationV1>,
     signal_sender: mpsc::SyncSender<Signal>,
 }
 
@@ -22,6 +25,7 @@ impl State {
     pub const fn new(signal_sender: mpsc::SyncSender<Signal>) -> Self {
         Self {
             idle_notifier: None,
+            idle_notification: None,
             signal_sender,
         }
     }
@@ -36,28 +40,49 @@ impl wayland_client::Dispatch<wl_registry::WlRegistry, ()> for State {
         _conn: &wayland_client::Connection,
         queue_handle: &wayland_client::QueueHandle<Self>,
     ) {
-        if let wl_registry::Event::Global {
-            name, interface, ..
-        } = event
-        {
-            match interface.as_str() {
+        match event {
+            wl_registry::Event::Global {
+                name, interface, ..
+            } => match interface.as_str() {
                 "wl_seat" => {
+                    // TODO: Support newest version of wl_seat.
                     registry.bind::<wl_seat::WlSeat, _, _>(name, 1, queue_handle, ());
+
+                    eprintln!("Binded to wl_seat");
                 }
                 "ext_idle_notifier_v1" => {
-                    state.idle_notifier = Some(
+                    state.idle_notifier = Some((
+                        name,
                         registry.bind::<ext_idle_notifier_v1::ExtIdleNotifierV1, _, _>(
                             name,
                             1,
                             queue_handle,
                             (),
                         ),
-                    );
+                    ));
 
                     eprintln!("Binded to ext_idle_notifier_v1");
                 }
                 _ => {}
+            },
+            wl_registry::Event::GlobalRemove { name } => {
+                if let Some((idle_notifier_name, idle_notifier)) = &state.idle_notifier {
+                    if name == *idle_notifier_name {
+                        idle_notifier.destroy();
+                        state.idle_notifier = None;
+
+                        eprintln!("Destroyed ext_idle_notifier_v1");
+
+                        if let Some(idle_notification) = &state.idle_notification {
+                            idle_notification.destroy();
+                            state.idle_notification = None;
+
+                            eprintln!("Destroyed ext_idle_notification_v1");
+                        }
+                    }
+                }
             }
+            _ => {}
         }
     }
 }
@@ -71,13 +96,14 @@ impl wayland_client::Dispatch<wl_seat::WlSeat, ()> for State {
         _conn: &wayland_client::Connection,
         queue_handle: &wayland_client::QueueHandle<Self>,
     ) {
-        if let Some(idle_notifier) = &state.idle_notifier {
-            idle_notifier.get_idle_notification(
+        // FIX: Support multiseat configuration.
+        if let Some((_, idle_notifier)) = &state.idle_notifier {
+            state.idle_notification = Some(idle_notifier.get_idle_notification(
                 CONFIG.timer.idle_timeout * 1000, // milli seconds
                 seat,
                 queue_handle,
                 (),
-            );
+            ));
 
             eprintln!("Created ext_idle_notification_v1");
         }
